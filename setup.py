7c9454986eb01777d5292d72ca729e3ef37104d2
Rob Hislop <rhislop@pandora.com>
2016-09-09 16:23:36 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+__version__ = '0'
+setup(
+    name='TradingTools',
+    version=__version__,
+    author='Rob Innes Hislop',
+    author_email='robinneshislop@gmail.com',
+    packages=find_packages(),
+    url = 'https://github.com/innes213/TradingTools',
+    description='Various tools that analyze and screen equities',
+    long_description='Various tools that analyze and screen equities'
+)
