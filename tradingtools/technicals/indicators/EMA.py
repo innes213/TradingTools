77ef4fe7d96f7fd4a401a210d8fb41f704c7cac5
innes213 <innes213@yahoo.com>
2016-12-31 23:43:49 -0800

    EMA and SMA analysis

--- tradingtools/technicals/indicators/EMA.py
+++ tradingtools/technicals/indicators/EMA.py
@@ -1,10 +1,13 @@
-from ..indicators import Indicator
+from ..indicators import Indicator, SignalStrengthTypes, SignalTypes
+from ...analysis import trend_length_and_slope
 from tradingtools.utils.equitydata import PastQuoteDataKeys
+
+from numpy import abs
 
 from datetime import datetime
 
 WINDOW_SIZE = 20
-NUM_PERIODS = 1
+NUM_PERIODS = 10
 
 class EMA(Indicator):
 
@@ -33,9 +36,7 @@ class EMA(Indicator):
         :return: List of Floats
         """
         if len(historic_data) <= self._window_size:
-            # todo: make this an exception
-            print 'Not enough data to calulate ema!'
-            return []
+            raise ValueError('Not enough data to calulate ema!')
 
         if len(historic_data) < 5 * self._window_size:
             print 'Warn: EMA may not have enouugh history to be valid'
@@ -59,3 +60,24 @@ class EMA(Indicator):
         :return: List of Floats
         """
         return self.calculate(self._data_for_symbol(symbol, self._num_periods + 5 * self._window_size, end_date, key))[-self._num_periods:]
+
+    def analyze(self, data):
+        trend_length, slope = trend_length_and_slope(data)
+        signal_type = SignalTypes.NEUTRAL
+        signal_strength = SignalStrengthTypes.NA
+        if trend_length > 1:
+            if slope > 0:
+                signal_type = SignalTypes.BULLISH
+            elif slope < 0:
+                signal_type = SignalTypes.BEARISH
+            magnitude = abs(slope)
+            if magnitude > 1.0:
+                signal_strength = SignalStrengthTypes.STRONG
+            elif magnitude > 0.1:
+                signal_strength = SignalStrengthTypes.MODEST
+            else:
+                signal_strength = SignalStrengthTypes.WEAK
+        return dict(signal_strength=signal_strength, signal_type=signal_type, trend_length=trend_length, slope=slope)
+
+    def analyze_for_symbol(self, symbol, end_date=datetime.today()):
+        return self.analyze(self.calculate_for_symbol(symbol, end_date))
--- tradingtools/technicals/indicators/SMA.py
+++ tradingtools/technicals/indicators/SMA.py
@@ -1,12 +1,13 @@
-from ..indicators import Indicator
+from ..indicators import Indicator, SignalStrengthTypes, SignalTypes
+from ...analysis import trend_length_and_slope
 from tradingtools.utils.equitydata import PastQuoteDataKeys
 
-from numpy import mean
+from numpy import abs, mean
 
 from datetime import datetime
 
 WINDOW_SIZE = 20
-NUM_PERIODS = 1
+NUM_PERIODS = 10
 
 class SMA(Indicator):
 
@@ -46,3 +47,24 @@ class SMA(Indicator):
         :return: List of floats
         """
         return self.calculate(self._data_for_symbol(symbol, self._num_periods + self._window_size - 1, end_date, key))
+
+    def analyze(self, data):
+        trend_length, slope = trend_length_and_slope(data)
+        signal_type = SignalTypes.NEUTRAL
+        signal_strength = SignalStrengthTypes.NA
+        if trend_length > 1:
+            if slope > 0:
+                signal_type = SignalTypes.BULLISH
+            elif slope < 0:
+                signal_type = SignalTypes.BEARISH
+            magnitude = abs(slope)
+            if magnitude > 1.0:
+                signal_strength = SignalStrengthTypes.STRONG
+            elif magnitude > 0.1:
+                signal_strength = SignalStrengthTypes.MODEST
+            else:
+                signal_strength = SignalStrengthTypes.WEAK
+        return dict(signal_strength=signal_strength, signal_type=signal_type, trend_length=trend_length, slope=slope)
+
+    def analyze_for_symbol(self, symbol, end_date=datetime.today()):
+        return self.analyze(self.calculate_for_symbol(symbol, end_date))
